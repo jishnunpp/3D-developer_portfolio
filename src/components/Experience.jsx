1098959bb6364bf341893ace44c42ee4f46d7172
jishnunpp <jishnunp397@gmail.com>
2023-10-03 11:23:19 +0530

    hero section added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import { VerticalTimeline,VerticalTimelineElement } from "react-vertical-timeline-component";
+impoert {motion} from 'framer-motion';
+import 'react-vertical-timeline-component/style.min.css';
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
 
 const Experience = () => {
   return (
